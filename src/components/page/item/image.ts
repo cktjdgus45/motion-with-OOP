5580733abf1824ceb676bcd0c091c1d5c9a741e4
pooh <cktjdgus45@naver.com>
2021-12-18 10:11:01 +0900

    add Note,Todo Component

--- src/components/page/item/image.ts
+++ src/components/page/item/image.ts
@@ -5,13 +5,13 @@ export class ImageComponent extends BaseComponent<HTMLElement>{
         super(`
         <section class="image">
             <div class="image__holder"><img class="image__thumbnail"></div>
-            <p class="image__title"></p>
+            <h2 class="image__title"></h2>
         </section>`);
         const imageElement = this.element.querySelector('.image__thumbnail')! as HTMLImageElement;
         imageElement.src = url;
         imageElement.alt = title;
 
-        const titleElement = this.element.querySelector('.image__title')! as HTMLParagraphElement;
+        const titleElement = this.element.querySelector('.image__title')! as HTMLHeadElement;
         titleElement.textContent = title;
     }
     //attachTo는 부모에게 상속 , this.element 또한 부모에게 상속.
