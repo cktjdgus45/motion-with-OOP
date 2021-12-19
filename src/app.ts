4024cfb5144f9eaec9acf51c06b04106eefebbf5
pooh <cktjdgus45@naver.com>
2021-12-19 20:29:55 +0900

    add dialog content

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,7 @@
 import { Component } from './components/component.js';
 import { InputDialog } from './components/dialog/dialog.js';
+import { MediaSectionInput } from './components/dialog/input/media-input.js';
+import { TextSectionInput } from './components/dialog/input/text-input.js';
 import { ImageComponent } from './components/page/item/image.js';
 import { NoteComponent } from './components/page/item/note.js';
 import { TodoComponent } from './components/page/item/todo.js';
@@ -11,29 +13,78 @@ class App {
     constructor(appRoot: HTMLElement) {
         this.page = new PageComponent(PageItemComponent);
         this.page.attachTo(appRoot);
-        const image = new ImageComponent('Image Title', 'https://picsum.photos/600/300');
-        this.page.addChild(image);
 
-        const video = new VideoComponent('Video Title', "https://www.youtube.com/embed/Jm5v-dZnYCs");
-        this.page.addChild(video);
 
-        const note = new NoteComponent('Note Title', 'Note Body');
-        this.page.addChild(note);
 
-        const todo = new TodoComponent('Todo Title', 'Todo');
-        this.page.addChild(todo);
+
+
+
+
+
 
         const imageBtn = document.querySelector('#new-image')! as HTMLButtonElement;
         imageBtn.addEventListener('click', () => {
             const dialog = new InputDialog();
+            const inputSection = new MediaSectionInput();
+            dialog.addChild(inputSection);
+            dialog.attachTo(document.body);
+
             dialog.setOncloseListener(() => {
                 dialog.removeFrom(document.body);
             })
             dialog.setOnsubmitListener(() => {
-                //섹션을 만들어서 페이지에 추가해준다.
+                const image = new ImageComponent(inputSection.title, inputSection.url);
+                this.page.addChild(image);
                 dialog.removeFrom(document.body);
             })
+        })
+        const videoBtn = document.querySelector('#new-video')! as HTMLButtonElement;
+        videoBtn.addEventListener('click', () => {
+            const dialog = new InputDialog();
+            const inputSection = new MediaSectionInput();
+            dialog.addChild(inputSection);
             dialog.attachTo(document.body);
+
+            dialog.setOncloseListener(() => {
+                dialog.removeFrom(document.body);
+            })
+            dialog.setOnsubmitListener(() => {
+                const video = new VideoComponent(inputSection.title, inputSection.url);
+                this.page.addChild(video);
+                dialog.removeFrom(document.body);
+            })
+        })
+        const noteBtn = document.querySelector('#new-note')! as HTMLButtonElement;
+        noteBtn.addEventListener('click', () => {
+            const dialog = new InputDialog();
+            const inputSection = new TextSectionInput();
+            dialog.addChild(inputSection);
+            dialog.attachTo(document.body);
+
+            dialog.setOncloseListener(() => {
+                dialog.removeFrom(document.body);
+            })
+            dialog.setOnsubmitListener(() => {
+                const note = new NoteComponent(inputSection.title, inputSection.body);
+                this.page.addChild(note);
+                dialog.removeFrom(document.body);
+            })
+        })
+        const todoBtn = document.querySelector('#new-todo')! as HTMLButtonElement;
+        todoBtn.addEventListener('click', () => {
+            const dialog = new InputDialog();
+            const inputSection = new TextSectionInput();
+            dialog.addChild(inputSection);
+            dialog.attachTo(document.body);
+
+            dialog.setOncloseListener(() => {
+                dialog.removeFrom(document.body);
+            })
+            dialog.setOnsubmitListener(() => {
+                const todo = new TodoComponent(inputSection.title, inputSection.body);
+                this.page.addChild(todo);
+                dialog.removeFrom(document.body);
+            })
         })
     }
 
