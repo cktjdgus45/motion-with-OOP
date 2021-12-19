0578ab01b0cc6822b71a23bc533ff3385efc30b5
pooh <cktjdgus45@naver.com>
2021-12-19 17:29:16 +0900

    add delete btn

--- src/components/component.ts
+++ src/components/component.ts
@@ -1,5 +1,6 @@
 export interface Component {
     attachTo(parent: HTMLElement, position?: InsertPosition): void;
+    removeFrom(parent: HTMLElement): void;
 }
 
 export class BaseComponent<T extends HTMLElement> implements Component {
@@ -8,8 +9,15 @@ export class BaseComponent<T extends HTMLElement> implements Component {
         const template = document.createElement('template');
         template.innerHTML = htmlString;
         this.element = template.content.firstElementChild! as T;
+        console.log(template.content);
     }
     attachTo(parent: HTMLElement, position: InsertPosition = 'afterbegin') {
         parent.insertAdjacentElement(position, this.element);
+    }
+    removeFrom(parent: HTMLElement) {
+        if (parent !== this.element.parentElement) {
+            throw new Error('Parent miss');
+        }
+        parent.removeChild(this.element);
     }
 }
